61fa237f1e307febfd82981d19324d120badba46
PAWAN KUMAR <pawanpassion2016@gmail.com>
2021-12-13 13:56:36 +0530

    Initial commit

---
+++ COVID19_INDIA-master/app/src/main/java/com/shivank/covid19india/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shivank.covid19india
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //just a test for push
+    }
+}
